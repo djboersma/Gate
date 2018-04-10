8140b82e35a599345459337bc9d96373c5eafbe1
David Boersma <david.boersma@physics.uu.se>
2018-04-10 17:09:56 +0200

    specify tolerance with unit, boost fixes, config gate message manager

--- source/unit_tests/general/GateHounsfieldToMaterialBuilderTest.cc
+++ source/unit_tests/general/GateHounsfieldToMaterialBuilderTest.cc
@@ -2,6 +2,8 @@
 #include <GateHounsfieldToMaterialsBuilder.hh>
 // design flaw: in order to get "the" material detector base, we first need to construct "the" detector
 #include <GateMaterialDatabase.hh>
+#include <GateMessageManager.hh>
+#include <G4SystemOfUnits.hh>
 #include <string>
 
 #ifdef GATE_SOURCE_DIR
@@ -11,10 +13,10 @@
 #else
 #error "GATE_SOURCE_DIR not defined"
 #endif
-// #define BOOST_FILESYSTEM_NO_LIB 
+
 #include <boost/test/unit_test.hpp>
-// #include <boost/filesystem.hpp>
-// namespace bfs = boost::filesystem;
+#include <boost/filesystem.hpp>
+namespace bfs = boost::filesystem;
 
 struct HUfixture {
     HUfixture() : hu2mb(new GateHounsfieldToMaterialsBuilder),
@@ -23,9 +25,12 @@ struct HUfixture {
                   output_db_file("output_db_file.txt"),
                   output_HU_material_file("output_HU_material_file.txt"){
         BOOST_TEST_MESSAGE( "Doing: setup of HU fixture" );
-#if 1
+	GateMessageManager *messenger = GateMessageManager::GetInstance();
+	messenger->SetMessageLevel("Geometry",4);
+	messenger->SetMessageLevel("Core",4);
+#if 0
         theMaterialDatabase.AddMDBFile( GATE_SOURCE_DIR_STRING "/GateMaterials.db" );
-#elif 0
+#elif 1
         theMaterialDatabase.AddMDBFile( (bfs::path( GATE_SOURCE_DIR_STRING ) / bfs::path("GateMaterials.db") ).string() );
 #else
         bfs::path mdbpath;
@@ -35,6 +40,46 @@ struct HUfixture {
 #endif
         std::ofstream fmaterials(input_material_file);
         // copy from GateContrib/dosimetry/Radiotherapy/example2/data/SimpleMaterialsTable.txt
+	fmaterials
+            << "[Elements] " << std::endl
+            << "Hydrogen  Carbon Nitrogen Oxygen Sodium Magnesium Phosphor Sulfur " << std::endl
+            << "Chlorine Argon Potassium Calcium " << std::endl
+            << "Titanium Copper Zinc  Silver Tin  " << std::endl
+            << "[/Elements]" << std::endl
+            << "# ===============================================================================" << std::endl
+            << "# HU      H    C    N    O   Na  Mg   P   S   Cl  Ar  K   Ca  Ti  Cu Zn  Ag  Sn" << std::endl
+            << "# ===============================================================================" << std::endl
+            << " -1050    0    0  75.5 23.2  0   0    0   0   0  1.3  0    0  0   0  0   0   0      Air       " << std::endl
+            << "  -950  10.3 10.5  3.1 74.9 0.2  0   0.2 0.3 0.3  0  0.2   0  0   0  0   0   0      Lung" << std::endl
+            << "  -120  11.6 68.1  0.2 19.8 0.1  0    0  0.1 0.1  0   0    0  0   0  0   0   0    AT_AG_SI1    " << std::endl
+            << "  -82   11.3 56.7  0.9 30.8 0.1  0    0  0.1 0.1  0   0    0  0   0  0   0   0    AT_AG_SI2    " << std::endl
+            << "  -52   11.0 45.8  1.5 41.1 0.1  0   0.1 0.2 0.2  0   0    0  0   0  0   0   0    AT_AG_SI3    " << std::endl
+            << "  -22   10.8 35.6  2.2 50.9  0   0   0.1 0.2 0.2  0   0    0  0   0  0   0   0    AT_AG_SI4    " << std::endl
+            << "   8    10.6 28.4  2.6 57.8  0   0   0.1 0.2 0.2  0  0.1   0  0   0  0   0   0    AT_AG_SI5    " << std::endl
+            << "   19   10.3 13.4  3.0 72.3 0.2  0   0.2 0.2 0.2  0  0.2   0  0   0  0   0   0   SoftTissus   " << std::endl
+            << "   80    9.4 20.7  6.2 62.2 0.6  0    0  0.6 0.3  0  0.0   0  0   0  0   0   0 ConnectiveTissue" << std::endl
+            << "  120    9.5 45.5  2.5 35.5 0.1  0   2.1 0.1 0.1  0  0.1  4.5 0   0  0   0   0  Marrow_Bone01   " << std::endl
+            << "  200    8.9 42.3  2.7 36.3 0.1  0   3.0 0.1 0.1  0  0.1  6.4 0   0  0   0   0  Marrow_Bone02   " << std::endl
+            << "  300    8.2 39.1  2.9 37.2 0.1  0   3.9 0.1 0.1  0  0.1  8.3 0   0  0   0   0  Marrow_Bone03   " << std::endl
+            << "  400    7.6 36.1  3.0 38.0 0.1 0.1  4.7 0.2 0.1  0   0  10.1 0   0  0   0   0  Marrow_Bone04   " << std::endl
+            << "  500    7.1 33.5  3.2 38.7 0.1 0.1  5.4 0.2    0 0   0  11.7 0   0  0   0   0  Marrow_Bone05  " << std::endl
+            << "  600    6.6 31.0  3.3 39.4 0.1 0.1  6.1 0.2  0   0   0  13.2 0   0  0   0   0  Marrow_Bone06   " << std::endl
+            << "  700    6.1 28.7  3.5 40.0 0.1 0.1  6.7 0.2  0   0   0  14.6 0   0  0   0   0  Marrow_Bone07   " << std::endl
+            << "  800    5.6 26.5  3.6 40.5 0.1 0.2  7.3 0.3  0   0   0  15.9 0   0  0   0   0   Marrow_Bone08   " << std::endl
+            << "  900    5.2 24.6  3.7 41.1 0.1 0.2  7.8 0.3  0   0   0  17.0 0   0  0   0   0  Marrow_Bone09   " << std::endl
+            << "  1000   4.9 22.7  3.8 41.6 0.1 0.2  8.3 0.3  0   0   0  18.1 0   0  0   0   0  Marrow_Bone10   " << std::endl
+            << "  1100   4.5 21.0  3.9 42.0 0.1 0.2  8.8 0.3  0   0   0  19.2 0   0  0   0   0  Marrow_Bone11   " << std::endl
+            << "  1200   4.2 19.4  4.0 42.5 0.1 0.2  9.2 0.3  0   0   0  20.1 0   0  0   0   0  Marrow_Bone12   " << std::endl
+            << "  1300   3.9 17.9  4.1 42.9 0.1 0.2  9.6 0.3  0   0   0  21.0 0   0  0   0   0  Marrow_Bone13   " << std::endl
+            << "  1400   3.6 16.5  4.2 43.2 0.1 0.2 10.0 0.3  0   0   0  21.9 0   0  0   0   0  Marrow_Bone14   " << std::endl
+            << "  1500   3.4 15.5  4.2 43.5 0.1 0.2 10.3 0.3  0   0   0  22.5 0   0  0   0   0  Marrow_Bone15   " << std::endl
+            << "  1640   0   0     0   0    0   0   0    0    0   0   0    0  0    4  2  65  29  AmalgamTooth" << std::endl
+            << "  2300   0   0     0   0    0   0   0    0    0   0   0    0  100  0  0  0   0  MetallImplants" << std::endl
+            << "  3000   0   0     0   0    0   0   0    0    0   0   0    0  100  0  0  0   0  MetallImplants" << std::endl
+            << "" << std::endl
+            << std::endl;
+        fmaterials.close();
+	/*
         fmaterials
             << "# ===============================================================================" << std::endl
             << "[Elements] " << std::endl
@@ -52,6 +97,7 @@ struct HUfixture {
             << "  1640   0   0     0   0    0   0   0    0    0   0   0    0  0    4  2  65  29  AmalgamTooth" << std::endl
             << "  2300   0   0     0   0    0   0   0    0    0   0   0    0  100  0  0  0   0  MetallImplants" << std::endl
             << "  3000   0   0     0   0    0   0   0    0    0   0   0    0  100  0  0  0   0  MetallImplants" << std::endl;
+	*/
         std::ofstream fdensities(input_density_file);
         // copy from GateContrib/dosimetry/Radiotherapy/example2/data/Schneider2000DensitiesTable.txt
         fdensities
@@ -67,12 +113,13 @@ struct HUfixture {
             << "101\t1.076200" << std::endl
             << "1600\t1.964200" << std::endl
             << "3000\t2.8" << std::endl;
+        fdensities.close();
 
         hu2mb->SetMaterialTable(input_material_file);
-        hu2mb->SetDensityTable(input_material_file);
+        hu2mb->SetDensityTable(input_density_file);
         hu2mb->SetOutputMaterialDatabaseFilename(output_db_file);
         hu2mb->SetOutputHUMaterialFilename(output_HU_material_file);
-        hu2mb->SetDensityTolerance( 0.1 );
+        hu2mb->SetDensityTolerance( 0.1 * g/cm3);
         BOOST_TEST_MESSAGE( "Done: setup of HU fixture" );
     }
     ~HUfixture(){
@@ -105,13 +152,11 @@ BOOST_FIXTURE_TEST_SUITE( HounsfieldToMaterialTests, HUfixture )
 BOOST_AUTO_TEST_CASE( construct_destruct )
 {
     BOOST_TEST_MESSAGE( "Constructing table builder, not building any tables, destruct...");
-    BOOST_TEST_WARN( "Constructing table builder, not building any tables, destruct...");
 }
 
 BOOST_AUTO_TEST_CASE( normal_build )
 {
     BOOST_TEST_MESSAGE( "build standard simple table without crashing" );
-    BOOST_TEST_WARN( "build standard simple table without crashing" );
     hu2mb->BuildAndWriteMaterials();
 }
 
